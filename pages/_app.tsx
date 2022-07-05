9f46467b212b938299121fc5423b920b8612ea7c
Khanh Pham <phamk0528@gmail.com>
2022-07-05 08:42:08 +0700

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@definitions/chakra/theme";
+import "@styles/global.css";
+import { RecoilRoot } from "recoil";
+import { appWithTranslation } from "@i18n";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider theme={theme}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
